59a5825322e1420b74e84ddf798c559082f5d09a
Madhunala Nikhil <madhunalanikhil.201ec131@nitk.edu.in>
2022-07-19 18:15:09 +0530

    crypt edit

--- src/Components/Workdata.jsx
+++ src/Components/Workdata.jsx
@@ -3,8 +3,8 @@ import pro1 from '../assets/crypto.png'
 const ProjectCardData=[
     {
         imgsrc:pro1,
-        title:"Project Title",
-        text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+        title:"Coindeko",
+        text:"A Cryptocurrencies web application build using REACT and coinranking API integration. In this project one can see real time crypto prices along with charts data.These live charts are build using integration of API data and chart.js framework .All GLOBAL STATS including 100+ cryptocurrencies, daily trending News, all Exchanges are being listed time to time automatically using API. I personally use my app to check updates in crypto market",
         view:"",
         source:""
 
